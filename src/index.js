cfd3b90390921d0fd3111af10394274e9b866bec
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2024-11-28 11:51:00 -0300

    Feat: Adiciona suporte à autenticação de instâncias e status no gerenciamento

--- src/index.js
+++ src/index.js
@@ -25,11 +25,21 @@ app.post('/add-instance', (req, res) => {
         authStrategy: new NoAuth(),
     });
 
-    instances[instanceId] = { client, name, qrCode: '' };
+    instances[instanceId] = { client, name, qrCode: '', auth: false };
 
     client.on('qr', (qr) => {
         instances[instanceId].qrCode = qr;
         console.log(`QR RECEBIDO para a instância ${instanceId}`, qr);
+    });
+
+    client.on('authenticated', () => {
+        instances[instanceId].auth = true;
+        console.log(`Instância ${instanceId} autenticada com sucesso.`);
+    });
+
+    client.on('auth_failure', () => {
+        instances[instanceId].auth = false;
+        console.log(`Falha na autenticação da instância ${instanceId}.`);
     });
 
     client.once('ready', () => {
@@ -42,16 +52,18 @@ app.post('/add-instance', (req, res) => {
     res.send({
         id: instanceId,
         name: name,
+        auth: instances[instanceId].auth,
         message: 'Instância está inicializando e o QR Code será gerado em breve.',
     });
 });
 
-// Endpoint para listar todas as instâncias com seus QR Codes
+// Endpoint para listar todas as instâncias com seus QR Codes e status de autenticação
 app.get('/instances', (req, res) => {
     const instanceList = Object.keys(instances).map(id => ({
         id,
         name: instances[id].name,
         qrCode: instances[id].qrCode,
+        auth: instances[id].auth,
     }));
     res.send(instanceList);
 });
@@ -61,7 +73,12 @@ app.get('/instance/:id', (req, res) => {
     const { id } = req.params;
     const instance = instances[id];
     if (instance) {
-        res.send({ id, name: instance.name, qrCode: instance.qrCode });
+        res.send({
+            id,
+            name: instance.name,
+            qrCode: instance.qrCode,
+            auth: instance.auth,
+        });
     } else {
         res.status(404).send('Instância não encontrada');
     }
@@ -84,11 +101,15 @@ app.post('/send-message', (req, res) => {
     const { instanceId, number, message } = req.body;
     const instance = instances[instanceId];
     if (instance) {
-        instance.client.sendMessage(number, message).then(response => {
-            res.send(response);
-        }).catch(err => {
-            res.status(500).send(err);
-        });
+        if (instance.auth) {
+            instance.client.sendMessage(number, message).then(response => {
+                res.send(response);
+            }).catch(err => {
+                res.status(500).send(err);
+            });
+        } else {
+            res.status(403).send('Instância não autenticada');
+        }
     } else {
         res.status(404).send('Instância não encontrada');
     }
